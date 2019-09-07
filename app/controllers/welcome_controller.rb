710143fe10614994a199d91ac316c0b02cb5602f
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-06 19:26:11 -0500

    Done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,8 @@ class WelcomeController < ApplicationController
     def home
     end
 
-    def current_user
-        @user = (User.find_by(session[:user_id]) || User.new)
-    end
-
     def must_be_logged_in
+        @user = (User.find_by(session[:user_id]) || User.new)
         return redirect_to login_path unless current_user.id != nil
     end
 end
